23f823574e6e223bdbae0d822fc851ab0d4299f0
Mehdi-Lucas <mehdi.lucas.ammar@gmail.com>
2020-12-01 15:17:03 +0100

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
   resources :appointment, only: [:index, :new, :create, :destroy]
+  ressources :place, only: [:index, :show, :create, :edit, :update]
 end
