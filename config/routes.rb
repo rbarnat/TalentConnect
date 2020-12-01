61de38e8fb087de33bc9554ddbd2046d176c1a99
Mehdi-Lucas <mehdi.lucas.ammar@gmail.com>
2020-12-02 00:42:36 +0100

    added appointement creation through view and fixed controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
+  # Appointment creation is accessible through the talent show page 
   resources :talents
-  resources :appointment, only: [:index, :new, :create, :destroy]
-  resources :place, only: [:index, :show, :create, :edit, :update]
+  resources :talents, only: [:show] do 
+    resources :appointments, only: [:new, :create]
+  end
+  resources :appointments, only: [:index, :destroy]
+  resources :places, only: [:index, :show, :create, :edit, :update]
   resources :static_pages, only: [:home, :about, :contact]
 end
