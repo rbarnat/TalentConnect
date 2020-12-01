4f2f95a0d722b1f10ded941fa11c01700aa512a9
Yohan Fleury <yfleury95@gmail.com>
2020-12-01 12:14:32 +0100

    first commit of pg_seachr

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -11,4 +11,7 @@ validates :description,
     presence: true,
     length: { in: 10..1000, message: "Description length must be between 10 and 1000"}
 
+# --- PG_SEARCH : search talents by key words ---
+    include PGSearch::Model 
+    pg_search_scope :search_by_title, against: :title
 end
