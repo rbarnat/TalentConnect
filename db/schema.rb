77520ca56c0ec64cd0c65297eca00de1cfba97b6
Rémi BARNAT <rbarnat@gmail.com>
2020-12-01 21:34:24 +0400

    shema merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_12_01_120401) do
-=======
 ActiveRecord::Schema.define(version: 2020_12_01_133244) do
->>>>>>> aea29dafcbec9c0fd999979ab18930e4d6b9c10b
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
