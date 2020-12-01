526b2ded7f92e8976626005a38c352e05cfee725
Rémi BARNAT <rbarnat@gmail.com>
2020-12-01 23:35:17 +0400

    Fix static pages routes + comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  # HOME PAGE
+  root to: "static_pages#home"
+  # USERS AND PROFILE PAGES
   devise_for :users
   resources :users, only: [:index, :show]
+  # TALENTS PAGES
+  resources :talents do
+    resources :pictures, only: [:create]
+  end
+  # APPOINTMENTS PAGES
   resources :appointment, only: [:index, :new, :create, :destroy]
+  # PLACES PAGES
   resources :place, only: [:index, :show, :create, :edit, :update]
-  resources :static_pages, only: [:home, :about, :contact]
+  # STATIC PAGES : HOME ABOUT CONTACT
+  get "/home" => "static_pages#home"
+  get "/about" => "static_pages#about"
+  get "/contact" => "static_pages#contact"
 end
