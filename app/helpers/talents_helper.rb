d2a96040202adf02723ec0641179774fe2d87a1c
Mehdi-Lucas <mehdi.lucas.ammar@gmail.com>
2020-12-03 12:15:30 +0100

    added helper

--- app/helpers/talents_helper.rb
+++ app/helpers/talents_helper.rb
@@ -1,2 +1,8 @@
 module TalentsHelper
+  def user_have_info?
+    if current_user.first_name.nil?
+      flash[:danger] = "Tu n'a pas encore reseigner ton prénom"
+      redirect_to root_path
+      end
+  end
 end
