c6d10bddc3577ca6dcf0392bf371d5402c8a096b
Rémi BARNAT <rbarnat@gmail.com>
2020-12-03 17:25:35 +0400

    users views only if login + profil page basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
- 
+  before_action :authenticate_user!, only: [:show, :index]
+  before_action :redirect_if_user_not_current_user
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def is_user_current_user?
+    if user_signed_in?
+      @user = User.find(params[:id])
+      return user_signed_in? && @user.id == current_user.id
+    else
+      return false
+    end
+  end
+
+  def redirect_if_user_not_current_user
+    if !is_user_current_user?
+      flash[:error] = "Tu n'as pas l'autisation de gérer ce profil."
+      redirect_to root_path
+    end
   end
 
 end
