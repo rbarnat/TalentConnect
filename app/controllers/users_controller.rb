db593c11f7a7feb6600ddc47b5de4f184d20717b
Yohan Fleury <yfleury95@gmail.com>
2020-12-03 13:52:23 +0100

    add some before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :check_user
  
   def index
     @users = User.all
@@ -8,5 +9,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  private
+
+  def check_user
+    if current_user != @user
+      redirect_to root_url, alert: "Tu ne peux pas voir ce profil, il appartient à quelqu'un d'autre."
+    end
+  end
 end
 
