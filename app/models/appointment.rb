62fa1e1e382fc62cdf56cc824f93cbf99f020006
PatriceH <patricehuszezo@gmail.com>
2020-12-04 17:36:36 +0100

    Adding confirmation email to apprentice at booking of talent session

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,15 @@
 class Appointment < ApplicationRecord
-    validates :start_time, presence: true
+  validates :start_time, presence: true
     
-    belongs_to :mentor, class_name: "User"
-    belongs_to :apprentice, class_name: "User"
-    belongs_to :place
-    belongs_to :talent
+  belongs_to :mentor, class_name: "User"
+  belongs_to :apprentice, class_name: "User"
+  belongs_to :place
+  belongs_to :talent
 
+  after_create :booking_user_send
+
+  def booking_user_send
+    UserMailer.booking_user_confirmation(self).deliver_now
+  end
+  
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,8 +2,12 @@
 class UserMailerPreview < ActionMailer::Preview
   def welcome_email
     # Set up a temporary user for the preview
-    user = User.new(email:"previewmailer@yopmail.com", password:"previewmailer")
-
+    user = User.new(email:"previewmailer@yopmail.com", password:"previewmailer", first_name:"John")
     UserMailer.welcome_email(user)
+  end
+
+  def booking_user_confirmation
+    appointment = Appointment.find(1)
+    UserMailer.booking_user_confirmation(appointment)
   end
 end
