4f491e4baf9b9fac5249bf2501a695105d6d9c5a
Rémi BARNAT <rbarnat@gmail.com>
2020-12-08 23:10:43 +0400

    SEED PLACES + USERS + CATEGORIES + TALENTS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,6 @@ Rails.application.configure do
 
   # Configuration Mailer
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
