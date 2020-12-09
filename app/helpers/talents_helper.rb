b2cfa68897f58edfb0f0cdb9ee3e1ab6deafd0d6
Mehdi-Lucas <mehdi.lucas.ammar@gmail.com>
2020-12-09 23:50:49 +0100

    access is resticted for review, user, appointment and talent, fixed some front for new review too

--- app/helpers/talents_helper.rb
+++ app/helpers/talents_helper.rb
@@ -1,13 +1,21 @@
 module TalentsHelper
+  private 
   def user_have_info?
     if current_user.first_name.nil?
       flash[:danger] = "Tu n'a pas encore renseigner ton prénom"
-      redirect_to edit_user_registration_path # a changer par user edit path quand cette page sera ajoutée
-      end
+      redirect_to edit_user_registration_path
+    end
   end
 
   def convert_price(price)
     price * 100
+  end
+
+  def is_current_user_talent?
+    unless current_user.id == Talent.find(params[:id]).user_id
+      flash[:danger] = "Tu ne peux pas éditer le talent d'une autre personne"
+      redirect_to talent_path(params[:id])
+    end
   end
 
   def global_mark(appointments)
