c2ebf3ca523a15f8b04924c08ce32ee71b71c8b6
Yohan Fleury <yfleury95@gmail.com>
2020-12-11 16:57:32 +0100

    no update

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,17 +1,30 @@
 class Admin::CategoriesController < ApplicationController
 
     def index
-        @categories = Category.all
+        @category = Category.all
     end
 
     def create
-        
+        @category = Category.new(category_params)
+        @category.save
+        if @category.save
+            redirect_to admin_talents_path ,  alert: 'Catégorie créée avec succés !'
+        else
+            redirect_to admin_talents_path , alert: 'La création de la catégorie a échouée'
+        end
+
     end
 
     def destroy 
-        @categorie = Category.find(params[:id])
-        @categorie.destroy
+        @category = Category.find(params[:id])
+        @category.destroy
         redirect_to admin_talents_path ,  alert: 'Catégorie supprimée !'
+    end
+
+    private
+
+    def category_params
+        params.require(:title)
     end
     
 end
