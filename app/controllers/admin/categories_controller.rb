7d700c4b010023eaaefd9973e3b9e24824b0dc48
Yohan Fleury <yfleury95@gmail.com>
2020-12-11 17:45:01 +0100

    admin dashboard ok

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,25 +6,23 @@ class Admin::CategoriesController < ApplicationController
 
     def create
         @category = Category.new(category_params)
-        @category.save
         if @category.save
-            redirect_to admin_talents_path ,  alert: 'Catégorie créée avec succés !'
+            redirect_to admin_talents_path ,  success: 'Catégorie créée avec succés !'
         else
-            redirect_to admin_talents_path , alert: 'La création de la catégorie a échouée'
+            redirect_to admin_talents_path , danger: 'La création de la catégorie a échouée'
         end
-
     end
 
     def destroy 
         @category = Category.find(params[:id])
         @category.destroy
-        redirect_to admin_talents_path ,  alert: 'Catégorie supprimée !'
+        redirect_to admin_talents_path ,  success: 'Catégorie supprimée !'
     end
 
     private
 
     def category_params
-        params.require(:title)
+        params.require(:category).permit(:title)
     end
     
 end
